264e2fc60b49ae5af6d0a6471ff60c14551909d1
Langley <langley@gmail.com>
2022-03-17 07:48:03 +0800

    remove log

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./config/swagger.json");
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+// const logger = require('morgan');
 const cors = require('cors');
 const app = express();
 const corsConfig = require('./config/di').corsConfig;
 const corsOptions = corsConfig;
 
 app.use(cors(corsOptions));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
